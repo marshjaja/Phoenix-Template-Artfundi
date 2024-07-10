7d676c0296b2c35d1394477cbe54fe64747852d1
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-07-10 19:17:11 +0100

    update: Updated the swipers for latest exhibitions and latest news

--- js/swiper.js
+++ js/swiper.js
@@ -1,32 +1,90 @@
 document.addEventListener("DOMContentLoaded", function () {
 	//?  ========== Initialize Hero Swiper ===========
 	//?  ========== Initialize Hero Swiper ===========
-
-	var heroCarousel = document.getElementById("heroCarousel");
-	var rightNumbers = document.querySelectorAll(".right-numbers span");
-
-	heroCarousel.addEventListener("slide.bs.carousel", function (event) {
-		var activeIndex = event.to;
-		updateCustomIndicators(activeIndex);
+	var swiper = new Swiper("hero-swiper", {
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
 	});
 
-	rightNumbers.forEach(function (number, index) {
+	// Add click event listeners to custom span elements
+	const $numbers = document.querySelectorAll(".right-numbers span");
+
+	$numbers.forEach((number, i) => {
 		number.addEventListener("click", function () {
-			var carousel = bootstrap.Carousel.getInstance(heroCarousel);
-			carousel.to(index);
+			swiper.slideToLoop(i, 1000); // Use Swiper's slideTo method
 		});
 	});
 
-	function updateCustomIndicators(index) {
-		rightNumbers.forEach(function (number, i) {
-			number.classList.toggle("active", i === index);
+	// Update active class on custom span elements based on Swiper's active index
+	swiper.on("slideChange", function () {
+		const currentIndex = swiper.realIndex;
+		$numbers.forEach((number, i) => {
+			number.classList.toggle("active", i === currentIndex);
 		});
-	}
+	});
 
+	// Initially set the active class on the first number
+	$numbers[0].classList.add("active");
 	// NOTE
 	//?  ========== Initialize Latest Swiper ===========
 	//?  ========== Initialize Latest Swiper ===========
 	var swiper = new Swiper(".latest-swiper", {
+		slidesPerView: 4,
+		spaceBetween: 30,
+
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+			},
+			640: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			1280: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			1440: {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+			1600: {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+			1920: {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+		},
+		draggable: true,
+		watchOverflow: true,
+	});
+	var swiper = new Swiper(".latest-news", {
 		loop: true,
 		slidesPerView: 4,
 		spaceBetween: 30,
